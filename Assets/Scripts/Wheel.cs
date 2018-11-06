b3d531bd79af688b3584ac72deca1152cf9e3445
Abregana <iaa3@illinois.edu>
2018-11-05 19:35:04 -0600

    Initial Commit

---
+++ Assets/Scripts/Wheel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wheel : MonoBehaviour {
+
+    public GameObject room;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //room.transform.rotation = transform.rotation;
+
+	}
+}
