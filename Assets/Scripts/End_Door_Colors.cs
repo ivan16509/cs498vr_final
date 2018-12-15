65f1e856e4a7c6e80782e9b7801717188f46eebd
Abregana <iaa3@illinois.edu>
2018-12-15 17:27:50 -0600

    added assets for lvl 1

---
+++ Assets/Scripts/End_Door_Colors.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class End_Door_Colors : MonoBehaviour {
+    public Light light;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Random.value > 0.9) //a random chance
+        {
+            light.intensity = Random.Range(0.3f, 1f); // makes the door flicker
+        }
+
+    }
+}
